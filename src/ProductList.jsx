2176575e953cff33aadea71e8d377c2657c910b8
ipm111 <ipm000@gmail.com>
2025-01-13 20:47:41 -0600

    commit ProductList1

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -268,7 +268,22 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-
+            {plantsArray.map((category,index) =>(
+                <div key={index}>
+                    <h1><div>{category.category}</div></h1>        
+                    <div className='product-list'>
+                        {category.plants.map((plant,plantindex) =>(
+                         <div className="product-card" key={plantindex}>
+                            <img className='product-image' src={plant.image} alt={plant.name}/>
+                            <div className='product-title'> {plant.name} </div>
+                            <button className='product-button' onClick={()=>handleAddToCart(plant)}>Add to Cart</button>
+                         </div>   
+                        )
+                    )}
+                    </div>
+                </div>                    
+            )
+            )}
 
         </div>
  ) :  (
